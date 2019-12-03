3164e309137e2de9c3e0c44ca7099775cac3aecc
Natasha Adler <natashaadler@Natashas-MacBook-Pro.local>
2019-12-03 00:36:47 -0500

    Created four graphs, added images, and text

---
+++ data/medalCount10Countries.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+import matplotlib.pyplot as plt 
+
+#Bar graph for top ten countries over all medal count - stacked graph
+
+N = 10
+bronzeMeans = (167, 107, 127, 93, 221, 177, 97, 132, 103, 79)
+silverMeans = (319, 203, 171, 97, 147, 129, 126, 77, 98, 90)
+goldMeans = (167, 315, 159, 250, 66, 127, 137, 76, 79, 94)
+ind = np.arange(N)
+width = 0.30
+
+p1 = plt.bar(ind, bronzeMeans, width)
+p2 = plt.bar(ind, silverMeans, width, bottom=bronzeMeans)
+p3 = plt.bar(ind, goldMeans, width, bottom=silverMeans)
+
+plt.ylabel(" Total Number Of Medals Won")
+plt.xlabel("Top Ten Countries")
+plt.title("Top Ten Countries - Total Medal Count")
+plt.xticks(ind, ("USA", "CAN", "NOR", "URS", "FIN", "SWE", "GER", "SUI", "AUT", "RUS"))
+plt.yticks(np.arange(0, 660, 50))
+plt.legend((p1[0], p2[0], p3[0]), ("gold", "Silver", "Bronze"))
+plt.show()
